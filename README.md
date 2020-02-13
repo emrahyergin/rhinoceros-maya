2a69c4eab9cb0499e6e883a62f13ad77282c399e
Emrah Yergin <emrahyergin@gmail.com>
2020-02-13 19:50:46 +0300

    upload local files

---
+++ README.md
@@ -0,0 +1,1 @@
+# rhinoceros-maya-py
---
+++ rhino_python.py
@@ -0,0 +1,90 @@
+# randomcubes.rhino
+
+import rhinoscriptsyntax as rs
+from Rhino import Geometry
+import random
+
+# seed_factor variable defined in grasshopper
+random.seed(seed_factor)
+
+# create a point
+somePoint = rs.coerce3dpoint(startPoint)
+
+# a list to store points
+x = []
+
+
+# Create a box
+
+def  boxgen(somePoint, recursion):
+	
+	x.append(somePoint)
+
+	# size variable defined in gh
+	move_x = random.uniform(-size*factor, size*factor)
+	move_y = random.uniform(-size*factor, size*factor)
+
+	# create stacked boxes 
+	while recursion < 5 :
+
+		# move the point in z
+		zPoint = somePoint + Point3d(0, size, size)
+		# create an instance
+		boxgen(somePoint, recursion + 1)
+		
+		if (abs(move_x) > size * 0.5) & (abs(move_y) > 0.5 * size):  
+
+			# move the point in y
+			yPoint = somePoint + Point3d(-move_x, -move_y, size)
+			# create an instance
+			boxgen(somePoint, recursion + 1)
+	
+
+# create an instance
+result = boxgen(somePoint, 0)
+
+# then boxes
+boxgen(result)
+
+
+### animation ###
+
+import scriptcontext as sc 
+
+# refer to the document
+sc.doc = ghdoc # grasshopper document
+
+# define the location, inputs in the gh gui
+loc = rs.coerce3dpoint(location)
+target = rs.coerce3dpoint(target)
+
+viewport = sc.doc.Views.Active.ActiveViewport
+
+viewport.SetCameraLocation(cameraLocation=loc, updateTargetLocation=False)
+viewport.SetCameraDirection(target - loc, updateTargetLocation=False)
+
+
+import Rhino
+
+while active:
+	for i in range(len(geo)):
+		
+		geo_id = geo[i]
+
+		# switch to gh context
+		sc.doc = ghdoc
+		doc_object = rs.coercerhinoobject(geo_id)
+		geometry = doc_object.Geometry
+		attributes = doc_object.Attributes
+
+		# switch to rhino context, and migrate the geom
+		sc.doc = Rhino.RhinoDoc.ActiveDoc
+		ref_rhino = sc.doc.Objects.Add(geometry, attributes)
+
+		# change the layer
+		rs.ObjectLayer(rhino_ref, layer)
+
+	else:
+		break
+
+# end of the script
